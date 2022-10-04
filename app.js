e0c21ef352fa0f249ade29c87b51755448d04e5c
Aldo320 <aldoantunez12@gmail.com>
2022-10-04 17:43:29 -0300

    first commit

--- app.js
+++ app.js
@@ -190,3 +190,21 @@ botonSorteo.addEventListener('click', () =>{
         Swal.fire(`Entered email: ${email}`)
       }  
     })
+
+const lista = document.querySelector("#listado")
+
+const pedirPost = async()=>{
+    const respuesta = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const data = await respuesta.json()
+
+    data.forEach((post)=>{
+        const li = document.createElement("li")
+        li.innerHTML=`
+        <h4>${post.title}</h4>
+        <h5>${post.body}</h5>
+        `
+        lista.append(li)
+    })
+}
+
+pedirPost()
